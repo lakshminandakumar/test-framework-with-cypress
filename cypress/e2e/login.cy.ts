df2d417ebf4fbf581a9f16e40c9afd4fe9ffb95b
Lakshmi Nandakumar <lakshmi.nandakumar@drvnsolutions.com>
2023-10-10 20:24:10 +0100

    added negative test cases for login

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -11,4 +11,18 @@ describe('Tools QA', () => {
         cy.get('button#login').click();
         cy.contains('Log out').should('exist');
     })
+
+    it('does not log in successfully with incorrect username', () => {
+        cy.get('#userName').type('demo.user1');
+        cy.get('#password').type('Dem0u$er');
+        cy.get('button#login').click();
+        cy.contains('Invalid username or password!').should('exist');
+    })
+
+    it('does not log in successfully with incorrect password', () => {
+        cy.get('#userName').type('demo.user');
+        cy.get('#password').type('Dem0u$er1');
+        cy.get('button#login').click();
+        cy.contains('Invalid username or password!').should('exist');
+    })
 })
