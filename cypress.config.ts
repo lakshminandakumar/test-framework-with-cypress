8ff30dcbaa6c9f58d14fe409e8cc37a862545134
Lakshmi Nandakumar <lakshmi.nandakumar@drvnsolutions.com>
2023-10-10 19:47:17 +0100

    configured cypress

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'cypress';
+
+async function setupNodeEvents(on, config){
+    
+}
+
+export default defineConfig({
+    e2e: {
+          retries: 1,
+          specPattern: "**/*.cy.ts",
+          baseUrl: "https://demoqa.com/login",
+      },
+      env: {
+          tags: 'not @wip'
+      },
+  });
