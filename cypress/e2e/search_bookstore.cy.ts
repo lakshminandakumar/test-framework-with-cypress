fcfd3e3667912c15c5a08b018b4d82aef7d9bcee
Lakshmi Nandakumar <lakshmi.nandakumar@drvnsolutions.com>
2023-11-06 20:43:35 +0000

    wip

--- cypress/e2e/search_bookstore.cy.ts
+++ cypress/e2e/search_bookstore.cy.ts
@@ -1,11 +1,14 @@
 describe('Searching Function in ToolsQA Book Store App', () => {
+    // let data;
     beforeEach(() => {
-        cy.visit('/books');
-        cy.loginViaApi();
-        // cy.get('input#searchBox').should('exist');
+        cy.session('login',() => {
+            cy.visit('/login');
+            cy.login(Cypress.env('validUsername'), Cypress.env('validPassword'));
+        })
+        cy.contains('span', 'Book Store').click();
     })
     it('search for a book', () => {
-        // cy.get('input#searchBox').clear().type('Git')
+        cy.get('input#searchBox').clear().type('Git')
         cy.get('input#searchBox').should('exist');
         cy.get('button#submit').should('be visible');
     })
